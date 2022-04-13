53608bfff87ea4794c5865c97f5f4de53d97cfad
extensaman <you@example.comverus.wedding@gmail.com>
2022-04-13 13:33:59 +0300

    Initial commit

---
+++ src/main/java/by/academy/it/task08/App.java
@@ -0,0 +1,15 @@
+package by.academy.it.task08;
+
+/**
+ * Connect to project jacoco plugin, checkstyle plugin, write tests, use hibernate.
+ * Add entities: Address(id, city, street, house) and People (id, first name, last name, patronymic)
+ * from the main method add, read, change and delete data in these tables
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
